0b59c16c0426048c2670474773b0ba529b37cb0d
Raquel Rosales <lraquelr6@gmail.com>
2023-05-05 22:44:29 -0400

    first-commit

--- src/app.py
+++ src/app.py
@@ -8,44 +8,203 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Favoritos_Personajes
 #from models import Person
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rickmorty.db'
+db.init_app(app)
 
 MIGRATE = Migrate(app, db)
-db.init_app(app)
-CORS(app)
-setup_admin(app)
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
+# generating all your endpoints
 @app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+def home():
+    return "Base Datos Rick and Morty"
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#Create new User
+@app.route('/users', methods=['POST'])
+def new_user():
+    user = User()
+    user.name = request.json.get('name')
+    user.lastname = request.json.get('lastname')
+    user.username = request.json.get('username')
+    user.email = request.json.get('email')
+    user.password = request.json.get('password')
 
-    return jsonify(response_body), 200
+    db.session.add(user)
+    db.session.commit()
+
+    return "user created"
+
+#Create new Character
+@app.route('/character', methods=['POST'])
+def new_character():
+    character = Character()
+    character.name = request.json.get('name')
+    character.status = request.json.get('status')
+    character.gender = request.json.get('gender')
+    character.species = request.json.get('species')
+    character.origin = request.json.get('origin')
+    character.image = request.json.get('image')
+
+    db.session.add(character)
+    db.session.commit()
+
+    return "character create"
+
+#Create new Favorite
+@app.route('/favorite', methods=['POST'])
+def new_favorite():
+    favorite = Favoritos_Personajes()
+    favorite.user_id= request.json.get('user_id')
+    favorite.personaje_id = request.json.get('personaje_id')
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return "saved favorite"
+
+
+#Read Users
+@app.route('/users/list', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    result = []
+    for user in users:
+        result.append(user.serialize())
+    return jsonify(result), 200
+    
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    if user is not None:
+        return jsonify(user.serialize())
+    else:
+        return jsonify("not found"), 404
+
+#Read Characters
+@app.route('/character/list', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    result = []
+    for character in characters:
+        result.append(character.serialize())
+    return jsonify(result), 200
+    
+@app.route('/character/<int:id>', methods=['GET'])
+def get_character(id):
+    character = Character.query.get(id)
+    if character is not None:
+        return jsonify(character.serialize())
+    else:
+        return jsonify("not found"), 404
+
+#Read Favorite
+@app.route('/favorites/list', methods=['GET'])
+def get_favorites():
+    favorites = Favoritos_Personajes.query.all()
+    result = []
+    for favorite in favorites:
+        result.append(favorite.serialize())
+    return jsonify(result), 200
+    
+@app.route('/favorite/<int:id>', methods=['GET'])
+def get_favorite(id):
+    favorite = Favoritos_Personajes.query.get(id)
+    if favorite is not None:
+        return jsonify(favorite.serialize())
+    else:
+        return jsonify("not found"), 404    
+
+#update/delete Users
+@app.route('/users/<int:id>', methods=['PUT', 'DELETE'])
+def update_user(id):
+    user = User.query.get(id)
+    if user is not None:
+        if request.method == "DELETE":
+            db.session.delete(user)
+            db.session.commit()
+
+            return jsonify(" "), 204
+        else:
+            user.name = request.json.get('name')
+            user.lastname = request.json.get('lastname')
+            user.username = request.json.get('username')
+            user.email = request.json.get('email')
+            user.password = request.json.get('password')
+            db.session.commit()
+
+            return jsonify("updated user"), 200
+        
+    return jsonify("not found"), 404    
+
+#update/delete Characters
+@app.route('/character/<int:id>', methods=['PUT', 'DELETE'])
+def update_character(id):
+    character = Character.query.get(id)
+    if character is not None:
+        if request.method == "DELETE":
+            db.session.delete(character)
+            db.session.commit()
+
+            return jsonify(" "), 204
+        else:
+            character.name = request.json.get('name')
+            character.status = request.json.get('status')
+            character.gender = request.json.get('gender')
+            character.species = request.json.get('species')
+            character.origin = request.json.get('origin')
+            character.image = request.json.get('image')
+            db.session.commit()
+
+            return jsonify("updated character"), 200
+        
+    return jsonify("not found"), 404  
+
+#update/delete Favorites
+@app.route('/favorite/<int:id>', methods=['PUT', 'DELETE'])
+def update_favorite(id):
+    favorite = Favoritos_Personajes.query.get(id)
+    if favorite is not None:
+        if request.method == "DELETE":
+            db.session.delete(favorite)
+            db.session.commit()
+
+            return jsonify(" "), 204
+        else:
+            favorite.user_id = request.json.get('user_id')
+            favorite.personaje_id = request.json.get('personaje_id')
+
+            db.session.commit()
+
+            return jsonify("updated favorite"), 200
+        
+    return jsonify("not found"), 404  
+
+
+
+
+
+
+
+
+#@app.route('/user', methods=['GET'])
+#def handle_hello():
+
+ #   response_body = {
+  #      "msg": "Hello, this is your GET /user response "
+   # }
+
+    #return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
+
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+#favoritos_personajes= db.Table(
+ #   "favoritos_personajes",
+  #  db.Base.metadata,
+   # db.Column("user_id", ForeignKey("user.id")),
+    #db.Column("character_id", ForeignKey("character.id")),
+#)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    lastname = db.Column(db.String(250))
+    username = db.Column(db.String(250))
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    favorito = db.relationship("Favoritos_Personajes")
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            'id' : self.id,
+            'name' : self.name,
+            'lastname': self.lastname,
+            'username': self.username,
+            'email': self.email
+             }
+
+
+class Favoritos_Personajes(db.Model):
+     __tablename__ = 'favorito_personaje'
+     id = db.Column(db.Integer, primary_key=True)
+     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+     personaje_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+     
+     def serialize(self):
+        return{
+            'id': self.id,
+            'user_id': self.user_id,
+            'personaje_id': self.personaje_id,
         }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    status = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    species = db.Column(db.String(250))
+    origin =  db.Column(db.String(250))
+    image = db.Column(db.String(250))
+    favorito = db.relationship("Favoritos_Personajes")
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'status': self.status,
+            'gender': self.gender,
+            'species': self.species,
+            'origin' : self.origin,
+            'image' : self.image,
+        }
+
+
+
+
+
+    
+
+
+
+
+
